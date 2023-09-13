b2d3120a901fed46d79363d3cea0bb44c1795361
Divyaa Sharmaa <shdivya1310@gmail.com>
2023-09-13 16:47:18 +0530

    Bubble Sort

---
+++ bubble_sort.java
@@ -0,0 +1,40 @@
+package Sorting;
+import java.util.Scanner;
+
+public class bubble_sort {
+
+public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int num = sc.nextInt();
+        int[] arr = new int[num];
+        inputArr(arr);
+        bubbleSort(arr);
+        printArr(arr);
+    }
+
+    public static void inputArr(int[] arr){
+        for (int i = 0; i< arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+    }
+
+    public static void bubbleSort(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length-1-i; j++) {
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArr(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+}
